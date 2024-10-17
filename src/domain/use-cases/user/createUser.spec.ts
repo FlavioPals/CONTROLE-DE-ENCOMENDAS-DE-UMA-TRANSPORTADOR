cb73a7c30030300b046bfb399a2d4682ad9bd714
FlavioPals <flaviopals@hotmail.com>
2024-10-17 15:20:24 -0300

    created user test

---
+++ src/domain/use-cases/user/createUser.spec.ts
@@ -0,0 +1,52 @@
+import { InMemoryUserRepository } from "../../../../test/in-memory-userRepository";
+import {beforeEach, describe, expect, it} from "vitest";
+import { CreateUserUseCase } from "./createUser";
+
+let inMemoryUserRepository : InMemoryUserRepository
+let sut: CreateUserUseCase
+
+describe('Create User Use Case' ,() => {
+    beforeEach(() => {
+        inMemoryUserRepository = new InMemoryUserRepository()
+        sut = new CreateUserUseCase(inMemoryUserRepository)
+    })
+
+    it('should be able to create a new user' , async () => {
+        const {user} = await sut.execute({
+            name: 'John Doe',
+            cpf: '664.643.260-07',
+            password: '123456'
+        })
+
+        expect(user.id).toEqual(expect.any(String))
+        })   
+
+    it('should not be able to create a user with invalid cpf', async () => {
+        await expect(() => 
+            sut.execute({
+            name: 'John Doe',
+            cpf: '11111111',
+            password: '123456'
+        })
+        ).rejects.toBeInstanceOf(Error)
+    })
+
+    it('should not be able to create a user with same cpf', async () => {
+        await sut.execute({
+            name: 'John Doe',
+            cpf: '664.643.260-07',
+            password: '123456'
+        })
+
+        await expect(() => 
+            sut.execute({
+            name: 'John Doe',
+            cpf: '664.643.260-07',
+            password: '123456'
+        })
+        ).rejects.toBeInstanceOf(Error)
+    }
+)
+    })
+
+
