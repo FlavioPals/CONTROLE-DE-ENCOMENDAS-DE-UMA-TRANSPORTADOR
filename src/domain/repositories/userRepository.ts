2f2c92d7719fe354664e6ff9800be158bfc2f04e
FlavioPals <flaviopals@hotmail.com>
2024-10-17 15:19:31 -0300

    created the method find by id and delete for interface user repository

--- src/domain/repositories/userRepository.ts
+++ src/domain/repositories/userRepository.ts
@@ -2,4 +2,6 @@ import { User } from "../entities/user";
 
 export interface IUserRepository {
     create(user: User): Promise<void>
+    findById(id: string): Promise<User | null>
+    detele(userId:string, adminId: string): Promise<void>
 }
